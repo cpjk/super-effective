859c29ea66f506cdea308425b4afd9066c141175
cpjk <cpjkelly@gmail.com>
2014-08-17 12:37:51 -0700

    Add damage_factor_vs instance method to Type

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,6 +1,10 @@
 class Type < ActiveRecord::Base
   has_many :pokemon, through: :pokemon_types
   has_many :pokemon_types
-  has_many :damage_type_efficacies, class_name: 'TypeEfficacy', foreign_key: :damage_type_id # when the type is damage type. migration needed
-  has_many :target_type_efficacies, class_name: 'TypeEfficacy', foreign_key: :target_type_id # when the type is the target type. migration needed
+  has_many :damage_type_efficacies, class_name: 'TypeEfficacy', foreign_key: :damage_type_id # when the type is damage type
+  has_many :target_type_efficacies, class_name: 'TypeEfficacy', foreign_key: :target_type_id # when the type is the target type
+
+  def damage_factor_vs type = nil
+    self.damage_type_efficacies.find_by(target_type: type).damage_factor
+  end
 end
