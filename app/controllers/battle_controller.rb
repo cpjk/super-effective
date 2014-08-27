3bd499e1591b2e482571ab363c24c869f5b69f97
cpjk <cpjkelly@gmail.com>
2014-08-26 20:16:53 -0700

    Pokemon can be added and deleted from the team properly

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -1,5 +1,5 @@
 class BattleController < ApplicationController
-  before_filter :initialize_team
+  before_filter :update_team, only: :battle
 
   def battle
   end
@@ -7,28 +7,30 @@ class BattleController < ApplicationController
   def add_pokemon
     if session[:team].count >= 6
       flash[:error] = 'You already have a full team!'
-      redirect_to battle_path and return
-    end
-
-    pokemon_name = params[:add_pokemon].downcase
-    if Pokemon.find_by name: pokemon_name
-      session[:team] << pokemon_name
     else
-      flash[:error] = 'That is not a real pokemon!'
+      pokemon_name = params[:add_pokemon].downcase
+      if Pokemon.find_by name: pokemon_name
+        session[:team] << pokemon_name
+      else
+        flash[:error] = 'That is not a real pokemon!'
+      end
     end
-
+    update_team
     render 'battle'
   end
 
   def delete_pokemon
+    # remove pokemon passed in params from session[:team]
     session[:team].slice! session[:team].index(params[:pokemon])
+
+    update_team
     render 'battle'
   end
 
   private
 
-  def initialize_team
+  def update_team
     session[:team] ||= []
-    @team = session[:team]
+    @team = session[:team].map { |name| Pokemon.find_by name: name }
   end
 end
