7146b292c73b05b0ad2dc6faae9d45af9c161c8c
cpjk <cpjkelly@gmail.com>
2014-08-26 20:40:21 -0700

    Add naive pokemon selection method

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -33,4 +33,18 @@ class BattleController < ApplicationController
     session[:team] ||= []
     @team = session[:team].map { |name| Pokemon.find_by name: name }
   end
+
+  def best_pokemon enemy_name
+    enemy = Pokemon.find_by name: enemy_name
+    first_enemy_type = enemy.types.first
+    second_enemy_type = enemy.types.second
+
+    @team.sort_by do |member|
+      first_member_type = member.types.first
+      second_member_type = member.types.second
+
+      [first_member_type.damage_factor_as_damager_vs(first_enemy_type),
+       -first_enemy_type.damage_factor_as_damager_vs(first_member_type)]
+    end
+  end
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -4,7 +4,12 @@ class Type < ActiveRecord::Base
   has_many :damage_type_efficacies, class_name: 'TypeEfficacy', foreign_key: :damage_type_id # when the type is damage type
   has_many :target_type_efficacies, class_name: 'TypeEfficacy', foreign_key: :target_type_id # when the type is the target type
 
-  def damage_factor_vs type = nil
+  def damage_factor_as_damager_vs type = nil
     self.damage_type_efficacies.find_by(target_type: type).damage_factor
   end
+
+  def damage_factor_as_target_vs type = nil
+    self.target_type_efficacies.find_by(damage_type: type).damage_factor
+  end
+
 end
